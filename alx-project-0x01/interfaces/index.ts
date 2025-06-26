44e0e8db6d978186afcb07b0f0f529d5e3b5f165
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-06-26 14:50:14 +0300

    UserCard

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -8,24 +8,34 @@ export interface PostProps {
   body: string;
 }
 
+// interfaces/index.ts
 
-export interface UserProps {
-    id:       number;
-    name:     string;
-    username: string;
-    email:    string;
-    address:  { street:  string;
-                 suite:   string;
-                 city:    string;
-                 zipcode: string;
-                 geo:     { lat: string;
-                             lng: string;
-                           }
-               };
-    phone:    string;
-    website:  string;
-    company:  {name:        string;
-               catchPhrase: string;
-                bs:  string;};
+export interface Geo {
+  lat: string;
+  lng: string;
 }
 
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
