bcfe5c76e0f04e25988db63a4b9bf566b3bf3f16
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-06-26 18:04:22 +0300

    Add user modal

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -57,7 +57,7 @@ export interface PostModalProps {
 
 export interface UserModalProps {
   onClose: () => void;
-  onSubmit: (user: UserProps) => void;
+   onSubmit: (post: UserProps) => void;
 }
 
 export interface UserData {
